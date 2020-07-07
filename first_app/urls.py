ccb101cc68b2010cb6eaca6ca41eb976d99bce06
Albert Hsu <iverson52000@gmail.com>
2020-07-07 14:45:51 -0700

    added login and profile templates

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'first_app'
+
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    ]
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('signin', views.signin, name='signin'),
+    path('profile', views.profile, name='profile'),
+]
--- first_app/views.py
+++ first_app/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'first_app/index.html')
 
+def signin(request):
+	return render(request, 'first_app/signin.html')
+
+def register(request):
+	return render(request, 'first_app/register.html')
+
+def profile(request):
+	return render(request, 'first_app/profile.html')
+
